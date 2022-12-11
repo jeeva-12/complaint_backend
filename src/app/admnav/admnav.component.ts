4e5c588316861ed6d978961a1bb134a01711e67f
jeeva-12 <112372759+jeeva-12@users.noreply.github.com>
2022-12-11 15:19:21 +0530

    ds

---
+++ src/app/admnav/admnav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admnav',
+  templateUrl: './admnav.component.html',
+  styleUrls: ['./admnav.component.css']
+})
+export class AdmnavComponent {
+
+}
